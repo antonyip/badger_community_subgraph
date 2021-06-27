cea32ccd7c594a2865a39238c42cb80b52e0612c
Anton Yip <anton_yip@hotmail.com>
2021-06-26 20:22:10 -0700

    update yearn subgraphs

--- src/mapping.ts
+++ src/mapping.ts
@@ -1,8 +1,6 @@
-//export { handleShareTransfer, handleAffiliateShareTransfer } from './mappings/yVault';
-//export { handleHarvest } from './mappings/strategy';
-//export { handleStaked, handleUnstaked } from './mappings/geyser';
-//export { handleFarmHarvest } from './mappings/farm';
-//export { handleSushiHarvest } from './mappings/sushi';
+export { handleShareTransfer } from './mappings/yVault';
+export { handleAffiliateShareTransfer } from './mappings/yVault';
+export { handleHarvest } from './mappings/strategy';
 export { handleBadgerTransfer } from './mappings/badger';
-//export { handleDiggTransfer } from './mappings/digg';
+export { handleDiggTransfer } from './mappings/digg';
 
--- src/mappings/digg.ts
+++ src/mappings/digg.ts
@@ -1,8 +1,19 @@
 import { Address } from '@graphprotocol/graph-ts';
 
 import { Transfer, UFragments } from '../../generated/Digg/UFragments';
+import { SgTransfer } from '../../generated/schema'
+import { getEthNetwork } from '../utils/helpers/SG_network_helpers'
 import { getOrCreateTokenBalance } from '../utils/helpers/token/balance';
 export function handleDiggTransfer(event: Transfer): void {
+  // Record the Transfer
+  let sg_Transfer = new SgTransfer(event.transaction.hash.toHexString());
+  sg_Transfer.network = getEthNetwork();
+  sg_Transfer.from = event.transaction.from.toHexString();
+  sg_Transfer.to = event.transaction.to.toHexString();
+  sg_Transfer.amount = event.transaction.value;
+  sg_Transfer.save();
+  
+  // Update account values
   let diggToken = Address.fromString('0x798d1be841a82a273720ce31c822c61a67a601c3');
   let digg = UFragments.bind(diggToken);
   let fromId = event.params.from
